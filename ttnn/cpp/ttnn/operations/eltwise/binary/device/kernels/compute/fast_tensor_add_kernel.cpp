e8ef12c1418d0aed7f9f4013051ab2338024e05c
ravi688 <rp0412204@gmail.com>
2025-06-03 19:13:04 +0530

    The index should be passed into the template argument list for get_compile_time_arg_val<>

--- ttnn/cpp/ttnn/operations/eltwise/binary/device/kernels/compute/fast_tensor_add_kernel.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary/device/kernels/compute/fast_tensor_add_kernel.cpp
@@ -2,19 +2,22 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "compute_kernel_api.h"
+#include <cstdint>
+#include "compile_time_args.h"
+
+using namespace std;
 
 void kernel_main() {
-    uint32_t src0_addr = get_compile_time_arg_val<uint32_t>(0);
-    uint32_t src1_addr = get_compile_time_arg_val<uint32_t>(1);
-    uint32_t dst_addr = get_compile_time_arg_val<uint32_t>(2);
+    uint32_t src0_addr = get_compile_time_arg_val<0>();
+    uint32_t src1_addr = get_compile_time_arg_val<1>();
+    uint32_t dst_addr = get_compile_time_arg_val<2>();
 
     volatile uint32_t* src0_ptr = (uint32_t*)(src0_addr);
     volatile uint32_t* src1_ptr = (uint32_t*)(src1_addr);
 
     volatile uint32_t* dst_ptr = (uint32_t*)(dst_addr);
 
-    uint32_t num_elements = get_compile_time_arg_val<uint32_t>(3);
+    uint32_t num_elements = get_compile_time_arg_val<3>();
     for(uint32_t i = 0; i < num_elements; i++)
     	dst_ptr[i] = src0_ptr[i] + src1_ptr[i];
 }
