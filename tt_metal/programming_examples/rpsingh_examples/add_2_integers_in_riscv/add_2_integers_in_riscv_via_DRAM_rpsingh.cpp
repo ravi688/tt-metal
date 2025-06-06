b2b67c93fb489dfd5d33916ad3a3697bcb1a8cf0
ravi688 <rp0412204@gmail.com>
2025-06-06 18:00:08 +0530

    Added host and kernel/device code

---
+++ tt_metal/programming_examples/rpsingh_examples/add_2_integers_in_riscv/add_2_integers_in_riscv_via_DRAM_rpsingh.cpp
@@ -0,0 +1,139 @@
+#include <tt-metalium/host_api.hpp>
+#include <tt-metalium/device.hpp>
+#include <tt-metalium/tt_metal.hpp>
+#include <tt-metalium/allocator.hpp>
+
+#include <ostream>
+#include <vector>
+#include <utility>
+#include <chrono>
+#include <cassert>
+
+
+template<typename T>
+static std::ostream& operator <<(std::ostream& stream, const std::vector<T>& v) noexcept
+{
+	stream << "{ ";
+	for(std::size_t i = 0; i < v.size(); ++i)
+	{
+		stream << v[i];
+		if((i + 1) != v.size())
+			stream << ", ";
+	}
+	stream << " }";
+	return stream;
+}
+
+struct Result
+{
+	std::vector<uint32_t> output;
+	float enqueue_program_time;
+	float kernel_finish_wait_time;
+	float output_readback_time;
+};
+
+static Result add_uint32_vector(const std::vector<uint32_t>& input0, const std::vector<uint32_t>& input1, const uint32_t page_count) noexcept
+{
+	const uint32_t input0_buffer_size = input0.size() * sizeof(uint32_t);
+	const uint32_t input1_buffer_size = input1.size() * sizeof(uint32_t);
+	std::assert(input0.size() == input1.size());
+	std::assert(input1_buffer_size % page_count == 0);
+
+	// Reserve storage on L1 of core { 0, 0 }
+	// Partition the storage into 3 paritions
+	// Populate the input0 into partition #0 (host to L1)
+	// Populate the input1 into partition #1 (host to L1)
+	// Populate the output into partition #2 (host to L1)
+	// Read back the partition #2 to host's vector (L1 to host)
+
+	// Create Device
+	uint32_t device_id = 0;
+	tt::tt_metal::IDevice* device = tt::tt_metal::CreateDevice(device_id);
+
+	// Get Command Queue (id = 0)
+	tt::tt_metal::CommandQueue& command_queue = device->command_queue();
+
+	// Create DRAM buffer for input vector 0
+	tt::tt_metal::InterleavedBufferConfig dram_buffer_config
+	{
+		.device = device,
+		.size = input0_buffer_size,
+		.page_size = input0_buffer_size / page_count
+	};
+	std::shared_ptr<Buffer> input0_dram_buffer = tt::tt_metal::CreateBuffer(dram_buffer_config);
+
+	// Create DRAM buffer for input vector 1
+	std::shared_ptr<Buffer> input1_dram_buffer = tt::tt_metal::CreateBuffer(dram_buffer_config);
+
+	// Populate the DRAM buffers with input vectors
+	tt::tt_metal::EnqueueWriteBuffer(command_queue, input0_dram_buffer, input0, false);
+	tt::tt_metal::EnqueueWriteBuffer(command_queue, input1_dram_buffer, input1, false);
+
+	// Create DRAM buffer for output
+	std::shared_ptr<Buffer> output_dram_buffer = std::tt_metal::CreateBuffer(dram_buffer_config);
+
+	// Just a single core will perform the computation (addition)
+	CoreCoord single_core { 0, 0 };
+
+	// Create Program
+	tt::tt_metal::Program program = tt::tt_metal::CreateProgram();
+
+	// Create Compute Kernel
+	uint32_t l1_unreserved_base = device->allocator()->get_base_allocator_addr(tt::tt_metal::HalMemType::L1);
+	tt::tt_metal::ComputeConfig data_move_kernel_config { };
+	datamove_data_move_config.compile_args = std::vector<uint32_t>
+										{ 
+											page_count,
+											l1_unreserved_base,
+											input0.size(),
+											input0_dram_buffer->address(),
+											input1_dram_buffer->address(),
+											output_dram_buffer->address()
+										};
+	const std::string kernel_file_path = "tt_metal/programming_examples/rpsingh_examples/add_2_integers_in_riscv/kernels/data_move_kernel.cpp";
+	[[maybe_unused]] tt::tt_metal::KernelHandle kernel_handle = tt::tt_metal::CreateKernel(program, kernel_file_path, single_core, data_move_kernel_config);
+
+	auto start = std::chrono::steady_clock::now();
+	// Launch Program using Fast Dispatch
+	tt::tt_metal::EnqueueProgram(command_queue, program, false);
+	auto end = std::chrono::steady_clock::now();
+	auto enqueue_elapsed = std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(end - start).count();
+
+	// While the kernel is being executed, let's allocate output buffer on the host side
+	std::vector<uint32_t> output;
+	output.reserve(input0.size());
+
+	start = std::chrono::steady_clock::now();
+	// Wait for device to complete kernel execution
+	tt::tt_metal::Finish(command_queue);
+	end = std::chrono::steady_clock::now();
+	auto kernel_finish_wait_time = std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(end - start).count();
+
+	start = std::chrono::steady_clock::now();
+	// Read back from output DRAM buffer to the host vector
+	tt::tt_metal::EnqueueReadBuffer(command_queue, output_dram_buffer, output, true);
+	auto output_readback_time = std::chrono::duration_cast<std::chrono::duration<float, std::milli>>(end - start).count(); 
+
+	// Close the device
+	tt::tt_metal::CloseDevice(device);
+
+	return { std::move(output), enqueue_elapsed, kernel_finish_wait_time, output_readback_time };
+}
+
+
+int main()
+{
+	std::vector<uint32_t> input_values0 = { 1, 2, 3, 4, 5, 6 };
+	std::vector<uint32_t> input_values1 = { 7, 8, 9, 10, 11, 12 };
+
+	std::cout << "input_values0: " << input_values0 << "\n";
+	std::cout << "input_values1: " << input_values1 << "\n";
+
+	// Page Count = 4, so the page size will be 6 bytes as we have only 6 x 4 = 24 bytes of data
+	auto [output_values, enqueue_time, kernel_finish_wait_time, output_readback_time] = add_uint32_vector(input_values0, input_values1, 4);
+
+	std::cout << "output_values: " << output_values << "\n";
+	std::cout << "enqueue_time: " << enqueue_time << " ms\n";
+	std::cout << "kernel_finish_wait_time: " << kernel_finish_wait_time << " ms\n";
+	std::cout << "output_readback_time: " << output_readback_time << " ms" << std::endl;
+}
---
+++ tt_metal/programming_examples/rpsingh_examples/add_2_integers_in_riscv/kernels/data_move_kernel.cpp
@@ -0,0 +1,63 @@
+
+#include "dataflow_api.h"
+
+// On wormhole chip we have 12 DRAM banks
+static constexpr uint32_t DRAM_BANK_COUNT = 12;
+
+void read_dram_pages(uint32_t src_dram_address, uint32_t l1_addr, uint32_t page_count, uint32_t page_size)
+{
+	for(uint32_t i = 0; i < page_count; ++i)
+	{
+		uint32_t bank_id = page_count % DRAM_BANK_COUNT;
+		uint32_t dram_noc_addr = get_noc_addr_from_bank_id<true>(bank_id, src_dram_address);
+		noc_async_read(dram_noc_addr, l1_addr + i * page_size, page_size);
+	}
+	noc_async_read_barrier();
+}
+
+void write_dram_pages(uint32_t dst_dram_address, uint32_t l1_addr, uint32_t page_count, uint32_t page_size)
+{
+	for(uint32_t i = 0; i < page_count; ++i)
+	{
+		uint32_t bank_id = page_count % DRAM_BANK_COUNT;
+		uint32_t dram_noc_addr = get_noc_addr_from_bank_id<true>(bank_id, dst_dram_address);
+		noc_async_write(l1_addr + i * page_size, dram_noc_addr, page_size);
+	}
+	noc_async_write_barrier();
+}
+
+void kernel_main()
+{
+	// Number of pages in which the inputs and outputs are divided
+	uint32_t page_count = get_compile_time_arg_val(0)
+	// L1 base address
+	uint32_t l1_mem_addr = get_compile_time_arg_val(1);
+	// element counts in the input vectors
+	uint32_t input_size = get_compile_time_arg_val(2);
+	uint32_t input0_dram_address = get_compile_time_arg_val(3);
+	uint32_t input1_dram_address = get_compile_time_arg_val(4);
+	uint32_t output_dram_address = get_compile_time_arg_val(5);
+
+
+	uint32_t input_size_in_bytes = input_size * sizeof(uint32_t);
+	uint32_t input0_l1_address = l1_mem_addr;
+	uint32_t input1_l1_address = l1_mem_addr + input_size_in_bytes;
+	uint32_t output_l1_address = input1_l1_address + input_size_in_bytes;
+
+	uint32_t page_size = input_size_in_bytes / page_count;
+
+	// Read input DRAM buffers into L1
+	read_dram_pages(input0_dram_address, input0_l1_address, page_count, page_size);
+	read_dram_pages(input1_dram_address, input1_l1_address, page_count, page_size);
+
+	uint32_t* input0_l1_ptr = (uint32_t*)input0_l1_address;
+	uint32_t* input1_l1_ptr = (uint32_t*)input1_l1_address;
+	uint32_t* output_l1_ptr = (uint32_t*)output_l1_address;
+
+	// Perform Addition
+	for(uint32_t i = 0; i < input_size; ++i)
+		output_l1_ptr[i] = input0_l1_ptr[i] + input1_l1_ptr[i];
+
+	// Write output DRAM buffer from L1
+	write_dram_pages(output_dram_address, output_l1_address, page_count, page_size);
+}
