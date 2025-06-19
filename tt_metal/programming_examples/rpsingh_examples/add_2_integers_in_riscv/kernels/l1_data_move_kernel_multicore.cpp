3318ee6535bb8f66024ca8cd6c2f581914d48fe3
ravi688 <rp0412204@gmail.com>
2025-06-19 20:51:13 +0530

    Added debug stub code

--- tt_metal/programming_examples/rpsingh_examples/add_2_integers_in_riscv/kernels/l1_data_move_kernel_multicore.cpp
+++ tt_metal/programming_examples/rpsingh_examples/add_2_integers_in_riscv/kernels/l1_data_move_kernel_multicore.cpp
@@ -12,6 +12,10 @@ void kernel_main()
 	uint32_t input1_l1_address = get_compile_time_arg_val(3);
 	uint32_t output_l1_address = get_compile_time_arg_val(4);
 
+	DPRINT << "input0_l1_address: "  << input0_l1_address << ENDL();
+	DPRINT << "input1_l1_address: "  << input1_l1_address << ENDL();
+	DPRINT << "output_l1_address: "  << output_l1_address << ENDL();
+
 
 	uint32_t per_core_input_size = input_size / page_count;
 
@@ -19,6 +23,10 @@ void kernel_main()
 	uint32_t* input0_l1_ptr = (uint32_t*)input0_l1_address;
 	uint32_t* input1_l1_ptr = (uint32_t*)input1_l1_address;
 	uint32_t* output_l1_ptr = (uint32_t*)output_l1_address;
+
+	DPRINT << "input0_l1_ptr: " << input0_l1_ptr[0] << ENDL();
+	DPRINT << "input1_l1_ptr: " << input1_l1_ptr[0] << ENDL();
+	DPRINT << "output_l1_ptr: " << output_l1_ptr[0] << ENDL();
 
 	// Perform Addition
 	for(uint32_t i = 0; i < per_core_input_size; ++i)
