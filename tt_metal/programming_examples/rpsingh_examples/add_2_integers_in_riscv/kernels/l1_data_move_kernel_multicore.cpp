ad2d48113e6c7061904c74a931aea452e14bd589
ravi688 <rp0412204@gmail.com>
2025-06-19 19:47:50 +0530

    Added host and kernel source files for multi-core vector addition using L1 buffers

---
+++ tt_metal/programming_examples/rpsingh_examples/add_2_integers_in_riscv/kernels/l1_data_move_kernel_multicore.cpp
@@ -0,0 +1,26 @@
+
+#include "dataflow_api.h"
+#include "debug/dprint.h"
+
+void kernel_main()
+{
+	// Number of pages in which the inputs and outputs are divided
+	uint32_t page_count = get_compile_time_arg_val(0);
+	// element counts in the input vectors
+	uint32_t input_size = get_compile_time_arg_val(1);
+	uint32_t input0_l1_address = get_compile_time_arg_val(2);
+	uint32_t input1_l1_address = get_compile_time_arg_val(3);
+	uint32_t output_l1_address = get_compile_time_arg_val(4);
+
+
+	uint32_t per_core_input_size = input_size / page_count;
+
+	// L1 Pointers are directly accessible to Baby RISC-V cores
+	uint32_t* input0_l1_ptr = (uint32_t*)input0_l1_address;
+	uint32_t* input1_l1_ptr = (uint32_t*)input1_l1_address;
+	uint32_t* output_l1_ptr = (uint32_t*)output_l1_address;
+
+	// Perform Addition
+	for(uint32_t i = 0; i < per_core_input_size; ++i)
+		output_l1_ptr[i] = input0_l1_ptr[i] + input1_l1_ptr[i];
+}
